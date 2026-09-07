fcf4638ccdc0329f67fe4ceb137817169b197418
Maria Tejaswi <mariatejaswiimandi@gmail.com>
2026-09-07 10:08:48 +0530

    api's done

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const app = express()
+// server ko start karna
+const app = require("./src/app")
 
-app.get("/", (req, res)=>{
-    res.send("Hello World")
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
 })
-
-app.get("/about", (req,res)=>{
-    res.send("About Page")
-})
-
-app.listen(3000)
---
+++ src/app.js
@@ -0,0 +1,51 @@
+// server ko create krna
+const express = require("express")
+const { dlopen } = require("process")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+//All four API's: 
+
+app.post('/notes', (req, res)=>{
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "note created successfully"
+    })
+})
+
+app.get('/notes', (req,res)=>{
+    res.status(200).json({
+        message: "notes fetched successfully",
+        notes: notes
+    })
+})
+
+app.delete('/notes/:index', (req,res)=>{
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(200).json({
+        message: "notes deleted successfully"
+    })
+})
+
+app.patch('/notes/:index', (req,res)=>{
+    const index = req.params.index
+    const description = req.body.description
+    const title = req.body.title
+
+    notes[index].description = description
+    notes[index].title = title
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
+})
+
+module.exports = app
